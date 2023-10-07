532bb0d4c96fae12b9e632ee7f9e3ce9a39f753f
gustavogarciaps <sousagustavogarcia@gmail.com>
2023-10-07 10:16:19 -0300

    Recurso: Download de Arquivos

--- backend/src/main/groovy/App.groovy
+++ backend/src/main/groovy/App.groovy
@@ -1,15 +1,20 @@
-import org.jsoup.Jsoup
+import model.ComponenteTISS
+import service.CrawlerService
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
 class App {
 
     static void main(args) {
-        String url = "https://www.gov.br/ans/pt-br/assuntos/prestadores/padrao-para-troca-de-informacao-de-saude-suplementar-2013-tiss/padrao-tiss-historico-das-versoes-dos-componentes-do-padrao-tiss"
+
+        final String URL_MAIN = "https://www.gov.br/ans/pt-br/assuntos/prestadores/padrao-para-troca-de-informacao-de-saude-suplementar-2013-tiss/" +
+                "padrao-tiss-historico-das-versoes-dos-componentes-do-padrao-tiss"
+        final String URL_SECOND = "https://www.gov.br/ans/pt-br/assuntos/prestadores/padrao-para-troca-de-informacao-de-saude-suplementar-2013-tiss/" +
+                "PadroTISS_ComponentedeContedoeEstrutura_202211.zip"
 
         try {
-            Document document = Jsoup.connect(url).get()
 
+            Document document = CrawlerService.getConnect(URL_MAIN);
 
             for (Element row in document.select("table tbody tr")) {
 
@@ -17,12 +22,14 @@ class App {
                 String publicacao = row.select("td:nth-of-type(2)").text()
                 String inicioVigencia = row.select("td:nth-of-type(3)").text()
 
-                println(new ComponentesTISS(competencia, publicacao, inicioVigencia))
+                println(new ComponenteTISS(competencia, publicacao, inicioVigencia))
             }
 
+            CrawlerService.downloadFile(URL_SECOND)
 
         } catch (Exception e) {
             println(e)
         }
+
     }
 }
---
+++ backend/src/main/groovy/model/ComponenteTISS.groovy
@@ -0,0 +1,26 @@
+package model
+
+class ComponenteTISS {
+
+    String competencia
+    String publicacao
+    String inicioVigencia
+
+    ComponenteTISS(String competencia,
+                   String publicacao,
+                   String inicioVigencia) {
+        this.competencia = competencia
+        this.publicacao = publicacao
+        this.inicioVigencia = inicioVigencia
+    }
+
+
+    @Override
+    public String toString() {
+        return "ComponentesTISS{" +
+                "competencia='" + competencia + '\'' +
+                ", publicacao=" + publicacao +
+                ", inicioVigencia=" + inicioVigencia +
+                '}';
+    }
+}
